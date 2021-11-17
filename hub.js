cb94f1d595bfa27785ad307cdf3ebaf28ea457b1
krashLanded <ericquesada02@icloud.com>
2021-11-16 16:17:00 -0800

    Multiple button creations

--- hub.js
+++ hub.js
@@ -3,12 +3,16 @@ const clearButton = document.createElement('button');
 clearButton.textContent = 'Clear';
 const eraseButton = document.createElement('button');
 eraseButton.textContent = 'Erase';
+const randomColorButton = document.createElement('button');
+randomColorButton.textContent = 'RanRGB';
 const buttonArea = document.querySelector('.buttonArea');
 buttonArea.appendChild(clearButton);
 buttonArea.appendChild(eraseButton);
+buttonArea.appendChild(randomColorButton);
 
-
-
+clearButton.classList.add('clearButton');
+eraseButton.classList.add('eraseButton');
+randomColorButton.classList.add('randomColorButton');
 
 
 
