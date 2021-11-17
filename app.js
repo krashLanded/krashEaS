22c60428ded97a1db7fa3554825b0a9ce38c7f8c
krashLanded <ericquesada02@icloud.com>
2021-11-16 16:13:14 -0800

    Created button functions

--- app.js
+++ app.js
@@ -2,13 +2,38 @@ for (i = 0; i < 256; i++) {
     let gridBox = document.createElement('div');
     container.appendChild(gridBox);
     gridBox.classList.add('gridBox');
-
 }
-
-const gridBox = document.querySelectorAll('.gridBox');
 
 [...document.querySelectorAll('.gridBox')].forEach(function(item) {
     item.addEventListener('mouseover', function() {
     item.style.backgroundColor = 'black';
     });
 });
+
+eraseButton.addEventListener('click', () => {
+        eraseMode = true;
+        alert('Click on app to exit erase mode!');
+        [...document.querySelectorAll('.gridBox')].forEach(function(item) {
+        item.addEventListener('mouseover', function() {
+        item.style.backgroundColor = 'white';
+        });
+    });
+});
+
+clearButton.addEventListener('click', () => {
+[...document.querySelectorAll('.gridBox')].forEach(function(item) {
+    item.style.backgroundColor = 'white';
+    });
+});
+;
+
+
+container.addEventListener('click', () => {
+    [...document.querySelectorAll('.gridBox')].forEach(function(item) {
+        item.addEventListener('mouseover', function() {
+        item.style.backgroundColor = 'black';
+        });
+    });
+});
+
+
